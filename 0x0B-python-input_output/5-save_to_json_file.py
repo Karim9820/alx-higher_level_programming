197ff95b7229bdae4a6dc4efd1d98dec02406848
Abdul Juma <karimjuma9820@gmail.com>
2023-01-10 19:33:27 +0000

    input output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,9 +5,6 @@ defines function
 """
 import json
 
-
 def save_to_json_file(my_obj, filename):
-    """write file"""
-    with open(filename, "w", encoding="utf=8") as fl:
-        """write obj file"""
-        json.dump(my_obj, fl)
+    with open(filename, 'w') as outfile:
+        json.dump(my_obj, outfile)
